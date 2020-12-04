95ff7fb14a4dd4eba71de5986238408460c05bcf
german <german@ws.agenciapaquetes.es>
2020-12-04 16:42:13 +0100

    Proyecto SD

--- api-rest-hoteles/routes/cars.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/api/coches', (req,res) =>{
-    res.send('Catálogo de ofertas de coches');
-});
-
-module.exports = router;
--- api-rest-hoteles/routes/hotels.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/api/hoteles', (req,res) =>{
-    res.send('Catálogo de ofertas de hoteles');
-});
-
-module.exports = router;
