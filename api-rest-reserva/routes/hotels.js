8ccfff2dd09613d6e2bde0382eba39307492d641
german <you@example.com>
2020-12-10 18:29:38 +0100

    Trabajo SD

--- api-rest-reserva/routes/hotels.js
+++ api-rest-reserva/routes/hotels.js
@@ -1,8 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const methodOverride = require("method-override");
 
-router.get('/api/hoteles', (req,res) =>{
-    res.send('Catálogo de ofertas de hoteles');
+const Hotel = require('../models/Hotel');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(methodOverride('_method'));
+
+const Transaccion = {
+readPreference: 'primary',
+readConcern:{ level: 'local'},
+writeConcern: {w: 'majority'}
+}
+
+router.get('/hoteles', async(req,res) =>{
+   
+    const hotels =  await Hotel.find().lean().sort({date: 'desc'});
+    res.render('hotels/see-hotel', {hotels});
+
 });
+
+router.get('/hoteles/modify', async(req,res) =>{
+   
+    const hotels =  await Hotel.find().lean().sort({date: 'desc'});
+    res.render('hotels/all-hotels', {hotels});
+
+});
+
+router.get('/hoteles/add', (req,res) =>{
+    res.render('hotels/new_hotel');
+});
+
+router.post('/hoteles/new-hotel', async(req, res) =>{
+    const {title, description}=req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text: 'Please Write a Title.'});
+    }
+    if(!description){
+        errors.push({text: 'Please Write a Description.'});
+    }
+    if(errors.length>0){
+        res.render('hotels/new_hotel', {
+            errors,
+            title,
+            description
+        });
+    }
+    else{
+        const newHotel = new Hotel({title,description});
+        await newHotel.save(Transaccion);
+        res.redirect('/hoteles/modify');
+    }
+});
+
+router.get('/hoteles/edit/:id', async(req, res) =>{
+    const hotel = await Hotel.findById(req.params.id).lean();
+    res.render('hotels/edit-hotel', {hotel});
+});
+
+router.put('/hoteles/edit-hotel/:id', async(req, res) =>{
+    const {title, description}=req.body;
+    await Hotel.findByIdAndUpdate(req.params.id, {title, description}).lean();
+    res.redirect('/hoteles/modify');
+});
+
+router.delete('/hoteles/delete/:id',async(req,res) =>{
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.redirect('/hoteles/modify');
+})
 
 module.exports = router;
--- api-rest-reserva/routes/users.js
+++ api-rest-reserva/routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const cookieParser = require('cookie-parser')
+const User = require('../models/User');
 
 router.use(cookieParser());
+
+
 router.get('/users/signin', (req,res) =>{
     res.render('users/Signin');
 });
@@ -10,6 +13,40 @@ router.get('/users/signin', (req,res) =>{
 router.get('/users/signup', (req,res) =>{
     res.render('users/Signup');
 });
+
+router.post('/users/signup', async (req,res) =>{
+    const{email,name,password,confirm_password}=req.body;
+    const errors = [];
+    if(email.length < 4){
+        errors.push({text: 'Email must be at least 4 characters'});
+    }
+    else if(name.length < 4){
+        errors.push({text: 'Nombre must be at least 4 characters'});
+    }
+    else if(password != confirm_password){
+        errors.push({text: 'Contraseña do not match'});
+    }
+    else if(password.length < 4){
+        errors.push({text: 'Contraseña must be at least 4 characters'});
+    }
+    if(errors.length != 0){
+        res.render('users/Signup',{errors,email,name,password,confirm_password});
+    }
+    else{
+        const emailUser = await User.findOne({email: email});
+        if(emailUser){
+            res.send('Email is in already in use!');
+        }
+        else{
+            const newUser = new User({name,email,password});
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            res.redirect('/users/Signin');
+        }
+
+    }
+
+})
 
 function validateCookie(req,res,next){
 
